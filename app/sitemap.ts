20062e2545a51bbc0b21806c8ce8878130a08341
musab-90dsep-code <musab-90dsep@dipti.com.bd>
2026-05-03 02:03:49 +0600

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,8 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://musabkhan.dev',
+      // ✅ এখানে আপনার আসল Vercel লিংক দেওয়া হলো
+      url: 'https://personal-portfolio-livid-psi-80.vercel.app',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
